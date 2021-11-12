fc076664c3c2dbadd0a959810e3da3a3e74de971
cambt1 <cambt@daum.net>
2021-11-12 18:09:40 +0900

    [update] mypager controller 양이슬

--- src/main/java/com/kh/eatsMap/myeats/controller/MyeatsController.java
+++ src/main/java/com/kh/eatsMap/myeats/controller/MyeatsController.java
@@ -1,10 +1,33 @@
 package com.kh.eatsMap.myeats.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("myeats")
 public class MyeatsController {
 
+	@GetMapping("/group")
+	public String groupView() {
+		return "mypage/group";
+	}
+	@GetMapping("/createGroup")
+	public String createGroupView() {
+		return "mypage/createGroup";
+	}
+	@GetMapping("/groupDetail")
+	public String groupDetailView() {
+		return "mypage/groupDetail";
+	}
+	@GetMapping("/post")
+	public String postView() {
+		return "mypage/post";
+	}
+	
+	@GetMapping("/detail")
+	public String detailView() {
+		return "mypage/detail";
+	}
+	
 }
