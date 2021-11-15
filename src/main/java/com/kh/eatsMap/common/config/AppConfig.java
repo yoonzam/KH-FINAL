a15fad046cca6e2ff7a6d90e17b46ae97c90c31a
yudii2 <qleen513@gmail.com>
2021-11-15 21:23:21 +0900

    [김유진] 몽고db 설정끝

--- src/main/java/com/kh/eatsMap/common/config/AppConfig.java
+++ src/main/java/com/kh/eatsMap/common/config/AppConfig.java
@@ -2,9 +2,7 @@ package com.kh.eatsMap.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.core.MongoClientFactoryBean;
 import org.springframework.data.mongodb.core.MongoTemplate;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -14,7 +12,7 @@ import com.mongodb.client.MongoClients;
 public class AppConfig {
 
   public @Bean MongoClient mongoClient() {
-      return MongoClients.create("mongodb+srv://won:won123@mongotest.w6gmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+      return MongoClients.create("mongodb+srv://yudi:yudi123@mongotest.w6gmc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
   }
 
   public @Bean MongoTemplate mongoTemplate() {
--- src/main/java/com/kh/eatsMap/member/model/dto/Member.java
+++ src/main/java/com/kh/eatsMap/member/model/dto/Member.java
@@ -4,14 +4,17 @@ import java.sql.Date;
 import java.time.LocalDate;
 
 import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import lombok.Data;
 
 @Data
+@Document
 public class Member {
 	
-	@MongoId
+	@Id
 	private ObjectId id;
 	private String nickname;
 	private String password;
