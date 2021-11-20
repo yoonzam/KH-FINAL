70ceb75c655116014b64f448e487df0ce79859b2
h0neyalpaca <dalcom0_0@naver.com>
2021-11-20 19:45:21 +0900

    [서우경] 후기등록 - 카카오API 장소검색 후 지도에 마커 표시 완료

--- src/main/java/com/kh/eatsMap/timeline/controller/TimelineController.java
+++ src/main/java/com/kh/eatsMap/timeline/controller/TimelineController.java
@@ -1,16 +1,48 @@
 package com.kh.eatsMap.timeline.controller;
 
+import java.net.URI;
+import java.net.URLEncoder;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 @RequestMapping("timeline")
 public class TimelineController {
 
+	/* 지도 장소검색 테스트 */
+	private static final String REST_API_KEY = "8f9fcce775dd72c4999049b1967438d8";
+	private static final String API_SERVER_HOST  = "https://dapi.kakao.com";
+    private static final String SEARCH_PLACE_KEYWORD_PATH = "/v2/local/search/keyword.json";
 	
+    public ResponseEntity<String> getSearchPlaceByKeyword(String keyword) throws Exception {
+    	
+        String queryString = "?sort=accuracy&query="+URLEncoder.encode(keyword, "UTF-8");
+        //String queryString = "?query="+URLEncoder.encode(keyword.getKeywordNm(), "UTF-8")+"&page="+keyword.getCurrentPage()+"&size="+keyword.getPageSize();
+        
+        RestTemplate restTemplate = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("Authorization", "KakaoAK " + REST_API_KEY);
+        headers.add("Accept", MediaType.APPLICATION_JSON_VALUE);
+        headers.add("Content-Type", MediaType.APPLICATION_FORM_URLENCODED_VALUE + ";charset=UTF-8");
+
+        URI url = URI.create(API_SERVER_HOST + SEARCH_PLACE_KEYWORD_PATH + queryString);
+        RequestEntity<String> rq = new RequestEntity<>(headers, HttpMethod.GET, url);
+        ResponseEntity<String> re = restTemplate.exchange(rq, String.class);
+        return re;
+    }
+    
 	@GetMapping("/")
-	public String timeline() {
+	public String timeline() throws Exception {
+		ResponseEntity<String> test = getSearchPlaceByKeyword("신림 떡볶이");
+		//System.out.println(test.);
 		return "timeline/timeline";
 	}
 }
--- src/main/webapp/resources/js/common/common.js
+++ src/main/webapp/resources/js/common/common.js
@@ -50,5 +50,64 @@ $('.dimmed').click(()=>{
     closePopup();
 });
 let closePopup = () => {
-    $('.dimmed-wrap').hide();
+    $('.dimmed-wrap').fadeOut(200);
 }
+
+/* 후기등록 */
+$('#btnReview').click(() => {
+	let searchPlaces;
+	
+	$('#pop-review-form').fadeIn(200);
+	let options = { center: new kakao.maps.LatLng(37.55317, 126.97279), level: 8 };
+	let map = new kakao.maps.Map(document.getElementById('uploadMap'), options);
+	
+	$('input[name="uploadLocation"]').keyup(function(){
+		let keyword = $(this).val();
+		$('.location-list').html('');
+		if(!keyword) return;
+		
+		// 장소 검색 객체 생성
+		let ps = new kakao.maps.services.Places();
+		// 키워드로 장소 검색
+		ps.keywordSearch(keyword, (data, status) => {
+			if (status === kakao.maps.services.Status.OK) displayPlaces(data);
+		}); 
+	});
+	
+	//검색리스트 출력
+	let displayPlaces = (places) => {
+		searchPlaces = places;
+		html = '';
+		for ( var i=0; i<places.length; i++ ) {
+			if(places[i].category_group_code == 'FD6' || places[i].category_group_code == 'CE7' )
+				html += '<li data-place-value="'+i+'"><span class="place-name">'+places[i].place_name+'</span> <span class="road-address-name">'+places[i].road_address_name+'</span></li>';
+		}
+		$('.location-list').html(html);
+		$('.location-list').show();
+	}
+	
+	//검색리스트 클릭시 지도에 표시
+	$('.location-list').click(function(e){
+		$('.location-list').hide();
+		let placeValue = e.target.dataset.placeValue;
+		drawSpecificMap(searchPlaces[placeValue]);
+	})
+	
+	//검색한 음식점 맵에 표시
+	let drawSpecificMap = (place) => {
+		let options = { center: new kakao.maps.LatLng(place.y, place.x), 
+						level: 5, 
+						keyboardShortcuts:true};
+		let map = new kakao.maps.Map(document.getElementById('uploadMap'), options);
+		let content = '<div class="marker-wrap"><p>'+place.place_name+'</p><div></div></div>';
+		
+		let customOverlay = new kakao.maps.CustomOverlay({
+		    position: new kakao.maps.LatLng(place.y, place.x),
+		    content: content,
+		    xAnchor: 0.3,
+		    yAnchor: 0.91
+		});
+		customOverlay.setMap(map);
+	}
+});
+	
