6e6ff3be28c2e9b34588ffa5aa051ad254d89057
weekband <gyqja15@gmail.com>
2021-12-07 04:32:12 +0900

    [문효범]map.jsp 초기화면 1.5km 근처 리뷰 리스트 출력,마이잇츠맵 기능 예외처리,소셜맵 기능 예외처리 추가

--- src/main/webapp/resources/js/map/Geolocation.js
+++ src/main/webapp/resources/js/map/Geolocation.js
@@ -4,7 +4,28 @@
  function getLocation() {
   if (navigator.geolocation) { // GPS를 지원하면
     navigator.geolocation.getCurrentPosition(function(position) {
-      alert(position.coords.latitude + ' ' + position.coords.longitude);
+     
+      	let lat = position.coords.latitude;
+      	let lng = position.coords.longitude;
+      	 
+
+      	let filterd = myEetsMap.filter((review)=>{
+				if(1.5 > getDistanceFromLatLonInKm(lat,lng,review.review.location.y,review.review.location.x)){
+					return true;
+				}
+				return false;
+				
+			});
+		console.dir(filterd);
+		if(filterd.length == 0){
+			 alert("근처의 리뷰가 없습니다.");
+		}else{
+			//원래 있는 지도에 마커 지우기
+			removeOverlay();
+			removeMarker();
+	    	markerCreate(filterd);
+		}
+		
     	
 		
       
@@ -19,5 +40,22 @@
     alert('GPS를 지원하지 않습니다');
   }
 }
+
+//자신과 리뷰음식점과의 거리 km리턴
+function getDistanceFromLatLonInKm(lat1,lng1,lat2,lng2) {
+    function deg2rad(deg) {
+        return deg * (Math.PI/180)
+    }
+
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(lat2-lat1);  // deg2rad below
+    var dLon = deg2rad(lng2-lng1);
+    var a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(dLon/2) * Math.sin(dLon/2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+    var d = R * c; // Distance in km
+    return d;
+}
+
+
 
 getLocation();
